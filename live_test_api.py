beaf06255ace8d3b760f013cf29d58b6587001bf
ShekharK <31747589+shekhark36@users.noreply.github.com>
2022-11-05 14:14:24 -0500

    Create live_test_api.py
    added live_test_api.py file

---
+++ live_test_api.py
@@ -0,0 +1,25 @@
+import json
+import requests
+
+data1 = {
+    "age": 39,
+    "workclass": "State-gov",
+    "fnlgt": 77516,
+    "education": "Bachelors",
+    "education_num": 13,
+    "marital_status": "Never-married",
+    "occupation": "Adm-clerical",
+    "relationship": "Not-in-family",
+    "race": "White",
+    "sex": "Male",
+    "capital_gain": 2174,
+    "capital_loss": 0,
+    "hours_per_week": 40,
+    "native_country": "United-States",
+    }
+
+response = requests.post('https://shekhar-project3-app.herokuapp.com/predict',
+                    data=json.dumps(data1))
+
+print(response.status_code)
+print(response.json())
