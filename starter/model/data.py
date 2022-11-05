799dea692fcd3a0925abdcfeec3b7a7a456623d5
ShekharK <31747589+shekhark36@users.noreply.github.com>
2022-11-05 13:29:50 -0500

    Update data.py
    updated data.py file

--- starter/model/data.py
+++ starter/model/data.py
@@ -1,19 +1,38 @@
+import pandas as pd
 import numpy as np
-from sklearn.preprocessing import LabelBinarizer, OneHotEncoder
+from sklearn.preprocessing import LabelBinarizer, OneHotEncoder, StandardScaler
 
+def clean_data(data: pd.DataFrame):
+
+    updated_cols = [col_name.strip() for col_name in list(data.columns)]
+    updated_cols = [col_name.replace("-", "_") for col_name in updated_cols]
+    data.columns = updated_cols
+
+    cat_features = [
+        "workclass",
+        "education",
+        "marital_status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native_country",
+    ]
+
+    for cat_col in cat_features:
+        data[cat_col] = data[cat_col].astype("category")
+
+    return data
 
 def process_data(
-    X, categorical_features=[], label=None, training=True, encoder=None, lb=None
+    X, categorical_features=[], label=None, training=True, encoder=None, scaler=None, lb=None
 ):
     """ Process the data used in the machine learning pipeline.
-
     Processes the data using one hot encoding for the categorical features and a
     label binarizer for the labels. This can be used in either training or
     inference/validation.
-
     Note: depending on the type of model used, you may want to add in functionality that
     scales the continuous data.
-
     Inputs
     ------
     X : pd.DataFrame
@@ -27,9 +46,10 @@ def process_data(
         Indicator if training mode or inference/validation mode.
     encoder : sklearn.preprocessing._encoders.OneHotEncoder
         Trained sklearn OneHotEncoder, only used if training=False.
+    scaler : sklearn.preprocessing._scaler.standardScaler
+        Trained sklearn standardScaler, only used if training=False.
     lb : sklearn.preprocessing._label.LabelBinarizer
         Trained sklearn LabelBinarizer, only used if training=False.
-
     Returns
     -------
     X : np.array
@@ -39,6 +59,7 @@ def process_data(
     encoder : sklearn.preprocessing._encoders.OneHotEncoder
         Trained OneHotEncoder if training is True, otherwise returns the encoder passed
         in.
+    scaler : 
     lb : sklearn.preprocessing._label.LabelBinarizer
         Trained LabelBinarizer if training is True, otherwise returns the binarizer
         passed in.
@@ -56,10 +77,13 @@ def process_data(
     if training is True:
         encoder = OneHotEncoder(sparse=False, handle_unknown="ignore")
         lb = LabelBinarizer()
+        scaler = StandardScaler()
         X_categorical = encoder.fit_transform(X_categorical)
+        X_continuous = scaler.fit_transform(X_continuous)
         y = lb.fit_transform(y.values).ravel()
     else:
         X_categorical = encoder.transform(X_categorical)
+        X_continuous = scaler.transform(X_continuous)
         try:
             y = lb.transform(y.values).ravel()
         # Catch the case where y is None because we're doing inference.
@@ -67,4 +91,4 @@ def process_data(
             pass
 
     X = np.concatenate([X_continuous, X_categorical], axis=1)
-    return X, y, encoder, lb
+    return X, y, encoder, scaler, lb
